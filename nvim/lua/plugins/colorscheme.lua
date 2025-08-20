1c94a30ea87a12391989e740bdadf3bd7893a387
raphael-weigand <raphael.weigand@gmail.com>
2025-08-20 20:40:18 +0200

    imprv: added white border for overlays

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,31 @@
 return {
-    "catppuccin/nvim", 
-    name = "catppuccin", 
-    priority = 1000, 
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
     config = function()
         require("catppuccin").setup({
-            flavour = "mocha", -- latte, frappe, macchiato, mocha
+            flavour = "mocha", 
             transparent_background = false,
-            -- Weitere Optionen nach Bedarf
         })
-        -- 1. Kein Farbschema laden
-        vim.cmd.colorscheme = ''
-
-        -- 2. Terminal-Farben explizit aktivieren
-        vim.o.termguicolors = false
-        -- vim.cmd.colorscheme "catppuccin"
+        
+        local function set_custom_highlights()
+            local white = "#ffffff"
+            
+            -- Weiße Overlay-Rahmen
+            vim.api.nvim_set_hl(0, "FloatBorder", { fg = white })
+            vim.api.nvim_set_hl(0, "LspInfoBorder", { fg = white })
+            vim.api.nvim_set_hl(0, "PmenuBorder", { fg = white })
+            vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = white })
+            vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = white })
+            vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { fg = white })
+            vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { fg = white })
+            vim.api.nvim_set_hl(0, "DiagnosticFloatingError", { fg = white })
+            vim.api.nvim_set_hl(0, "DiagnosticFloatingWarn", { fg = white })
+            vim.api.nvim_set_hl(0, "DiagnosticFloatingInfo", { fg = white })
+            vim.api.nvim_set_hl(0, "DiagnosticFloatingHint", { fg = white })
+        end
+        
+        -- Setze die Custom Highlights sofort
+        set_custom_highlights()
     end,
 }
-

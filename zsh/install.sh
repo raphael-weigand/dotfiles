7d8816d72ca81b8fb87a5f9cf84d1a9136f1c8ff
raphael-weigand <raphael.weigand@gmail.com>
2026-04-20 15:07:53 +0200

    Added autocomplete to zsh and install script

---
+++ zsh/install.sh
@@ -0,0 +1,5 @@
+echo "Install homebrew...";
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install dependencies
+brew install zsh-autosuggestions

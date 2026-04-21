10c0f40a22fb4d47d0eb08e1b06e689e338e46d0
raphael-weigand <raphael.weigand@gmail.com>
2026-04-21 16:42:35 +0200

    Neotree ignores cache files, .DS_Store etc.

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -10,13 +10,68 @@ return {
     opts = { },
     config = function()
         require("neo-tree").setup({
+            sources = {
+                "filesystem",
+                "buffers",
+                "git_status",
+            },
             close_if_last_window = true, -- Schließt Neo-tree, wenn es das letzte Fenster ist
             enable_git_status = true,
             enable_diagnostics = true,
-
+            enable_modified_markers = true,
+            enable_opened_markers = true,
+            enable_refresh_on_write = true, -- Refresh the tree when a file is written. Only used if `use_libuv_file_watcher` is false.
+            enable_cursor_hijack = false, -- If enabled neotree will keep the cursor on the first letter of the filename when moving in the tree.
+            git_status_async = true,
+            check_gitignore_in_search = false,
             window = {
                 position = "right", -- left, right, top, bottom, float, current
                 width = 40,
+            },
+            filesystem = {
+                filtered_items = {
+                    visible = false,
+                    hide_gitignored = false,
+                    hide_hidden = false,
+                    hide_dotfiles = false,
+                    never_show = {
+                        ".DS_Store",
+                        "node_modules",
+                        "Thumbs.db",
+                        ".git"
+                    },
+                    never_show_by_pattern = {
+                        "*.swp",
+                    },
+                    hide_by_pattern = {
+                        "*.pyc",
+                        "__pycache__",
+                    }
+                },
+            },
+            git_status = {
+                window = {
+                    mappings = {
+                        ["A"] = "git_add_all",
+                        ["gu"] = "git_unstage_file",
+                        ["gU"] = "git_undo_last_commit",
+                        ["ga"] = "git_add_file",
+                        ["gt"] = "git_toggle_file_stage",
+                        ["gr"] = "git_revert_file",
+                        ["gc"] = "git_commit",
+                        ["gp"] = "git_push",
+                        ["gg"] = "git_commit_and_push",
+                        ["i"] = "show_file_details", -- see `:h neo-tree-file-actions` for options to customize the window.
+                        ["b"] = "rename_basename",
+                        ["o"] = { "show_help", nowait=false, config = { title = "Order by", prefix_key = "o" }},
+                        ["oc"] = { "order_by_created", nowait = false },
+                        ["od"] = { "order_by_diagnostics", nowait = false },
+                        ["om"] = { "order_by_modified", nowait = false },
+                        ["on"] = { "order_by_name", nowait = false },
+                        ["os"] = { "order_by_size", nowait = false },
+                        ["ot"] = { "order_by_type", nowait = false },
+                    },
+                },
             },
         })
 

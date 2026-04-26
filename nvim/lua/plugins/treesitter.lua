c34dbd8bb13ff0b115ffc2881dcc90494df9ed0a
raphael-weigand <raphael.weigand@gmail.com>
2026-04-26 10:09:08 +0200

    Fixed error when K on function mulitple treesitter errors occured

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,22 +1,18 @@
 return {
-	"neovim-treesitter/nvim-treesitter",
+    "neovim-treesitter/nvim-treesitter",
     dependencies = { 'neovim-treesitter/treesitter-parser-registry' },
-	build = ":TSUpdate",
+    build = ":TSUpdate",
     priority = 1000,
-	lazy = false,
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true, disable = {"c"} },
-			ensure_installed = {"c", "lua", "javascript", "html", "css", "python"},
-			fold = { enable = true, disable={"c"}},
-		})
-
+    lazy = false,
+    config = function()
+        vim.api.nvim_create_autocmd("FileType", {
+            callback = function(args)
+                pcall(vim.treesitter.start, args.buf)
+            end,
+        })
         vim.opt.foldmethod = "expr"
         vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
         vim.opt.foldlevel = 99
         vim.opt.foldenable = true
-	end,
+    end,
 }

ed9fb3f4da72dbe3c366b776e314c394bab60808
raphael-weigand <raphael.weigand@gmail.com>
2026-05-16 13:16:20 +0200

    Added toggle to oil.nvim

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -45,8 +45,42 @@ return {
         prompt_save_on_select_new_entry = false,
     },
     keys = {
-        { "<C-n>", "<CMD>Oil<CR>", desc = "Open oil.nvim" },
+        {
+            "<C-n>",
+            function()
+                local oil = require("oil")
+
+                -- find oil windows
+                local oil_wins = {}
+
+                for _, win in ipairs(vim.api.nvim_list_wins()) do
+                    local buf = vim.api.nvim_win_get_buf(win)
+                    if vim.bo[buf].filetype == "oil" then
+                        table.insert(oil_wins, win)
+                    end
+                end
+
+                -- if Oil is open → close its window(s)
+                if #oil_wins > 0 then
+                    for _, win in ipairs(oil_wins) do
+                        -- avoid closing last window
+                        if #vim.api.nvim_list_wins() > 1 then
+                            vim.api.nvim_win_close(win, true)
+                        else
+                            -- fallback: just leave Oil buffer instead of closing window
+                            vim.cmd("b#")
+                        end
+                    end
+                    return
+                end
+
+                -- otherwise open Oil
+                oil.open()
+            end,
+            desc = "Toggle oil.nvim",
+        },
     },
+
     dependencies = { { "nvim-mini/mini.icons", opts = {} } },
     lazy = false,
 }

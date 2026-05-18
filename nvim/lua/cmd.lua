32f522c52e54bc6535c876fdfb53cab6088bcb93
raphael-weigand <raphael.weigand@gmail.com>
2026-05-18 12:33:59 +0200

    Added move of cursor horizontally in insert mode, added showing of hidden characters in visual mode

--- nvim/lua/cmd.lua
+++ nvim/lua/cmd.lua
@@ -7,6 +7,39 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     end,
 })
 
--- Visual mode
+-- Move lines up and down in visual mode
 vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv")
+
+-- Insert mode: move cursor
+vim.keymap.set("i", "<C-k>", "<Up>")
+vim.keymap.set("i", "<C-j>", "<Down>")
+vim.keymap.set("i", "<C-l>", "<Right>")
+vim.keymap.set("i", "<C-h>", "<Left>")
+
+local visual_list_group =
+  vim.api.nvim_create_augroup("VisualListChars", { clear = true })
+
+-- Enter Visual Mode
+vim.api.nvim_create_autocmd("ModeChanged", {
+  group = visual_list_group,
+  pattern = "*:[vV\22]*",
+  callback = function()
+    vim.opt_local.list = true
+  end,
+})
+
+-- Leave Visual Mode
+vim.api.nvim_create_autocmd("ModeChanged", {
+  group = visual_list_group,
+  pattern = "[vV\22]*:*",
+  callback = function()
+    vim.opt_local.list = false
+  end,
+})
+
+vim.opt.listchars = {
+  space = "·",
+  tab = "» ",
+  trail = "·",
+}

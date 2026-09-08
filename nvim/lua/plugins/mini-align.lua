674174ed5904632b515fc3f574ab565003d2f75e
whoami <57750576+raphael-weigand@users.noreply.github.com>
2026-09-08 16:13:08 +0200

    Add generic alignment shortcut

---
+++ nvim/lua/plugins/mini-align.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        "nvim-mini/mini.align",
+        version = "*",
+        opts = {
+            mappings = {
+                start = "<leader>a",
+                start_with_preview = "<leader>A",
+            },
+        },
+    },
+}

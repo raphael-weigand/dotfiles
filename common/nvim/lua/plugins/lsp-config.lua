ecec81b7b30cca1a3dd48d4438fc1032d20af96b
whoami <57750576+raphael-weigand@users.noreply.github.com>
2026-09-24 14:48:06 +0200

    Use distinct Mason package status icons

--- common/nvim/lua/plugins/lsp-config.lua
+++ common/nvim/lua/plugins/lsp-config.lua
@@ -1,7 +1,15 @@
 return {
     {
         "williamboman/mason.nvim",
-        opts = {},
+        opts = {
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "○",
+                },
+            },
+        },
     },
     {
         "williamboman/mason-lspconfig.nvim",
